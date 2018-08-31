f2cc9ac343ba484972d53994e6331b9fa4e79076
Jørgen Stamnes <jorgensta@gmail.com>
2018-08-31 12:04:04 +0200

    setup project structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../styles/components/App.css';
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
